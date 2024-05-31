2af7db752aa0d36afab7be64a852aee4daa4a7c2
whitedxke <whitedxke@gmail.com>
2024-06-01 01:40:11 +0300

    Initial commit.

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,9 @@
 import 'package:flutter/material.dart';
 
+import 'application.dart';
+
 void main() {
   runApp(
     const Application(),
   );
-}
-
-class Application extends StatelessWidget {
-  const Application({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
 }
